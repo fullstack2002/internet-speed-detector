cefb8c3de0fc4784675476255a80fb1629f6e66b
fullstack2002 <98652392+fullstack2002@users.noreply.github.com>
2023-03-06 00:56:54 +0200

    markup

---
+++ app.js
@@ -0,0 +1,6 @@
+let startTime, endTime;
+let imageSize = "";
+let image = new Image();
+let bitOutput = document.getElementById("bits");
+let kbsOutput = document.getElementById("kbs");
+let mbsOutput = document.getElementById("mbs");
