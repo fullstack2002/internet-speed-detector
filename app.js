50c97a3724a5b068d65f281c33c8349e4beb7224
fullstack2002 <98652392+fullstack2002@users.noreply.github.com>
2023-03-11 21:42:14 +0200

    js

--- app.js
+++ app.js
@@ -4,3 +4,30 @@ let image = new Image();
 let bitOutput = document.getElementById("bits");
 let kbsOutput = document.getElementById("kbs");
 let mbsOutput = document.getElementById("mbs");
+
+// get random image
+let imageLink = "https://source.unsplash.com/random?topics=arts&culture";
+console.log(imageLink);
+
+// when image loads
+image.onload = async function () {
+  endTime = new Date().getTime();
+
+// get image size 
+  await fetch(imageLink).then((response) => {
+    imageSize = response.headers.get("content-length");
+    calculateSpeed();
+  });
+};
+
+// calculate the speed
+function calculateSpeed() {
+  let timeDuration = (endTime - startTime) / 1000;
+
+  let loadedBits = imageSize * 8;
+  let speedInBps = (loadedBits / timeDuration).toFixed(2);
+  let speedInKbps = (speedInBps / 1024).toFixed(2);
+  let speedInMbps = (speedInKbps / 1024).toFixed(2);
+
+  
+}
